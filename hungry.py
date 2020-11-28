cdd468b76f35f110bf64943fba50c59a41516083
ujjwalpd008 <ujjwalpd008@gmail.com>
2020-11-28 16:25:23 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -1,6 +1,8 @@
 hungry=input("are you hungry?")
 if hungry = "yes":
     print("eat something")
-    print("eat")
+    print("eat pizza")
+    print("eat burger")
+    
 else:
     print("do something")
